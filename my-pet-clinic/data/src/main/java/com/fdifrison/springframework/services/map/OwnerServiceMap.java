6e9b1ef8029c28ced3cf9d4059ea7eaaa322c7a0
Giovanni Frison <ing.giovanni.frison@gmail.com>
2022-10-11 18:06:53 +0200

    Adding Boostrap data loader. Closes #11

--- my-pet-clinic/data/src/main/java/com/fdifrison/springframework/services/map/OwnerServiceMap.java
+++ my-pet-clinic/data/src/main/java/com/fdifrison/springframework/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.fdifrison.springframework.services.map;
 
 import com.fdifrison.springframework.model.Owner;
-import com.fdifrison.springframework.services.CrudService;
+import com.fdifrison.springframework.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void deleteByID(Long id) {
         super.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
