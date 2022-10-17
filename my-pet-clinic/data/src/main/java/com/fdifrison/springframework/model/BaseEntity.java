b20d48660eb80ba30221c3110eb3d0a7fad02622
Giovanni Frison <ing.giovanni.frison@gmail.com>
2022-10-17 17:08:56 +0200

    Added JPA mapping to BaseEntity. Closes #30

--- my-pet-clinic/data/src/main/java/com/fdifrison/springframework/model/BaseEntity.java
+++ my-pet-clinic/data/src/main/java/com/fdifrison/springframework/model/BaseEntity.java
@@ -1,9 +1,20 @@
 package com.fdifrison.springframework.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
+/*
+ We are saying to JPA that we don't need this class directly in the db but that there will be other classes
+built upon this one
+*/
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
