8e464309022886fbe85304c140f4a71cd0eb69ca
Giovanni Frison <ing.giovanni.frison@gmail.com>
2022-10-18 11:30:49 +0200

    JPa service for Owner. Closes #35

---
+++ my-pet-clinic/data/src/main/java/com/fdifrison/springframework/services/springdatajpa/OwnerJPA.java
@@ -0,0 +1,54 @@
+package com.fdifrison.springframework.services.springdatajpa;
+
+import com.fdifrison.springframework.model.Owner;
+import com.fdifrison.springframework.repositories.OwnerRepository;
+import com.fdifrison.springframework.repositories.PetRepository;
+import com.fdifrison.springframework.repositories.PetTypeRepository;
+import com.fdifrison.springframework.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class OwnerJPA implements OwnerService {
+
+    @Autowired
+    private OwnerRepository ownerRepository;
+    @Autowired
+    private PetRepository petRepository;
+    @Autowired
+    private PetTypeRepository petTypeRepository;
+
+    @Override
+    public Set<Owner> findAll() {
+        return (Set<Owner>) ownerRepository.findAll();
+    }
+
+    @Override
+    public Owner findByID(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        ownerRepository.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
